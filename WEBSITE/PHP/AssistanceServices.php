f204e5830e769d6906ebd7f44a24a10c283cd7a0
LucaMart <martinazzi.luca@gmai.com>
2016-06-19 14:12:23 +0200

    Updated Assistance services

--- WEBSITE/PHP/AssistanceServices.php
+++ WEBSITE/PHP/AssistanceServices.php
@@ -1,12 +1,12 @@
 <?php
 $servername='localhost';
 $username='user1';
-$passowrd='user1';
+$password='user1';
 $database='timdatabase';
 
 $connection = new mysqli($servername, $username, $password, $database);
 // Check connection
-if ($conn->connect_error) {
+if ($connection->connect_error) {
     print("Error connecting to DBMS:".mysqli_connect_error());
     exit();
 }
@@ -18,10 +18,12 @@ if($result->num_rows > 0){
     $myArray = array();
     $i = 0;
     while ($row = $result->fetch_assoc()){
-        $myArray[i] = $row;
-        i++;
+        $myArray[$i] = $row;
+        $i++;
     }
     $json = json_encode($myArray);
     echo $json;
 }
+$result->close();
+$connection->close();
 ?>
--- WEBSITE/Scripts/AssistanceServices.js
+++ WEBSITE/Scripts/AssistanceServices.js
@@ -1,4 +1,41 @@
-"use strict";
-function loadpage() {
-    document.getElementById("input").innerHTML = "Hello World";
+$(document).ready(fun);
+
+function fun(){
+    
+    $.ajax({
+            method: "POST",
+            url: "PHP/AssistanceServices.php",
+            success: function(response){
+                var categories = JSON.parse(response);
+                var i;
+                var lcol = "<div id=\"lcol\">";
+                var rcol = "<div id=\"rcol\">";
+                //TODO Highlgiths
+                for (i = 0; i < categories.length; i++){    
+                    //var imgPath = categories[i].;
+                    //var link = categories[i].;
+                    var title = categories[i].Category;
+                    if(i%2){
+                        rcol+="<div class=\"content\">";
+                        rcol+="<img class=\"asimg\" src=\"\" href=\"#\"/>";
+                        rcol+="<p class=\"title\">"+title+"</p>";
+                        rcol+="</div>";
+                    }else{
+                        lcol+="<div class=\"content\">";
+                        lcol+="<img class=\"asimg\" src=\"\" href=\"#\"/>";
+                        lcol+="<p class=\"title\">"+title+"</p>";
+                        lcol+="</div>";
+                    }
+                }
+                lcol+="</div>";
+                rcol+="</div>";
+                $('#contentContainer').append(lcol);
+                $('#contentContainer').append(rcol);
+            },
+            error: function(response, error){
+                $('#contentContainer').append("error");
+                console.log(error);
+            }
+    });
+    
 }
