f8f21ef63e571b15c191d82d013c649e88fdf99e
AndreaLK5 <Ibanez2093@live.com>
2016-06-19 12:51:30 +0200

    Setting up the Related Devices Page (need to study localStorage)

---
+++ WEBSITE/Scripts/RelatedDevices.js
@@ -0,0 +1,50 @@
+$(document).ready(loadPage);
+
+function loadPage(){
+    var id=1;
+
+    //READ THE VARIABLES ourType and ourServiceIdentifier FROM LOCAL STORAGE
+
+    $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        //crossDomain: true, //localhost purposes
+        url: "./PHP/RelatedDevices.php", //Relative or absolute path to file.php file
+        data: {
+            //SEND: type: ourType, serviceIdentifier: ourServiceIdentifier
+            },
+
+        success: function(response) {
+
+            console.log("Preparing to parse response...");
+            console.log(JSON.parse(response));
+            var devices = JSON.parse(response);
+            console.log("Response parsed successfully");
+
+            for(var i=0;i<devices.length;i++){
+                var name = devices[i].Name;
+                var price = devices[i].Price;
+                var imagePath = devices[i].ImagePath;
+
+                console.log(name);
+
+                listOfDevicesDivs+=
+                    "<div class= \"device col-xs-12 col-sm-3\">"+
+                    "<img src=\""+imagePath+"\">"+"</img>"+
+                    "<a href=\"Device.html?device="+name+"\""+">"+name+"</a>"+
+                    "<p>"+price+"</p>"+
+                    "</div>";
+
+            }
+
+            console.log(listOfDevicesDivs);
+            $('#mainReceiver').append(listOfDevicesDivs);
+
+        },
+        error: function(request,error)
+        {
+            console.log(error);
+        }
+    });
+
+}
