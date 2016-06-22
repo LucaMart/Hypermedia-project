8d34436c1f966a7c88d9530c572a6cf0f11c6aa7
LucaMart <martinazzi.luca@gmai.com>
2016-06-22 13:19:41 +0200

    Added filters

---
+++ WEBSITE/PHP/Filtertest.php
@@ -0,0 +1,42 @@
+<?php
+    $connection = mysqli_connect('localhost','user1','user1','timdatabase'); //establish a connection with the database
+
+    if (mysqli_connect_errno()){
+        print("Error connecting to DBMS:".mysqli_connect_error());
+        exit();
+    }
+    $radio = json_decode($_POST['radio']);
+    $checkbox = json_decode($_POST['checkbox']);
+    $field = "";
+    $i;
+
+    $chosenCategory = $_POST["category"];
+    if (!(is_string($chosenCategory))) {
+        print("Error : the variable $chosenCategory is not a string");
+        exit();
+    }
+    if(sizeof($checkbox)>0){
+        $field = $checkbox[0]->Name;
+        $query = "SELECT Name,ImagePath,Price,".$field;
+        $query= $query." FROM device WHERE Category='$chosenCategory'";
+            
+    }else{
+        $query = "SELECT Name,ImagePath,Price FROM device WHERE     Category='$chosenCategory'";
+    }
+    if($radio[0]->Value != "all"){
+        $value = $radio[0]->Value;
+        $name = $radio[0]->Name;
+        $query = $query." AND ".$name."='$value'";
+    }
+    $myArray = array(); //create an array to store result
+    $result = $connection->query($query);
+    if($result->num_rows >0){
+        while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $myArray[] = $row;
+        }
+    }
+    $jsonData = json_encode($myArray);
+    echo $jsonData; //export in json
+    $result->close();
+    $connection->close();
+?>
--- WEBSITE/Scripts/Devices-ViewCategory.js
+++ WEBSITE/Scripts/Devices-ViewCategory.js
@@ -1,4 +1,4 @@
-$(document).ready(loadPage);
+$(document).ready(filter);
 
 function loadPage(){
     var id=1;
@@ -48,4 +48,111 @@ function loadPage(){
         }
     });
 
+}
+var checkboxOn = [];
+var maxrange;
+var minrange;
+function filter(){
+    /*var radio = document.getElementsByName("deviceType");
+    var radioChecked;
+    var i;
+    //Search for checked button
+    for(i=0; i< radio.length; i++){
+        if(radio[i].checked)
+            radioChecked=radio[i].value;
+    }*/
+    var filters = document.getElementsByTagName('input');
+    var radioOn = [];
+    checkboxOn = [];
+    var c;
+    for(c=0; c < filters.length; c++){
+        if(filters[c].type=='radio' && filters[c].checked){
+            radioOn.push({Name: filters[c].name, Value: filters[c].value});
+        }else{
+            if(filters[c].type=='checkbox' && filters[c].checked){
+                    checkboxOn.push({Name: filters[c].name, Value: filters[c].value});
+            }else{
+                if(filters[c].type=='range'){
+                    if(filters[c].name=="minPrice"){
+                        minrange = filters[c].value;
+                        
+                    }
+                    else{
+                        if(filters[c].name=="maxPrice")
+                            maxrange = filters[c].value;
+                            
+                    }
+                }
+            }
+        }
+    }
+    $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        //crossDomain: true, //localhost purposes
+        url: "./PHP/Filtertest.php", //Relative or absolute path to file.php file
+        data: {
+            category : ourCategory,
+            checkbox : JSON.stringify(checkboxOn),
+            radio : JSON.stringify(radioOn),
+            },
+        success: function(response) {
+        var deviceList = JSON.parse(response);
+        var i;
+        var c;
+        var indexToRemove = [];
+        var listOfDevicesDivs="";
+        //$('h4').append(response);
+        $('.device').remove();
+        for(i=0; i< Object.keys(deviceList).length; i++){
+            //check the price value
+            var n = deviceList[i].Price.indexOf("&");
+            var stringPrice = deviceList[i].Price.substring(0,n);
+            stringPrice = stringPrice.replace(",",".");
+            var float = parseFloat(stringPrice);
+            if(float<minrange || float>maxrange){
+                deviceList.splice(i,1);
+                i--;
+            }else{
+                //check category
+               var remove = 0;
+                for(c=0; c<checkboxOn.length; c++){
+                    var string = checkboxOn[c].Name;
+                    var substring = checkboxOn[c].Value;
+                    if(deviceList[i][string].indexOf(substring) > -1){
+                        remove = 0;
+                        break;
+                    }
+                    remove =1;
+                }
+                if(remove==1){
+                    deviceList.splice(i,1);
+                    i--;
+                }
+            }
+        }
+        for(i=0;i<deviceList.length;i++){
+                var name = deviceList[i].Name;
+                var price = deviceList[i].Price;
+                var imagePath = deviceList[i].ImagePath;
+
+                //console.log(name);
+
+                listOfDevicesDivs+=
+                    "<div class= \"device col-xs-12 col-sm-3\">"+
+                    "<img src=\""+imagePath+"\">"+"</img>"+
+                    "<a href=\"Device.html?device="+name+"\""+">"+name+"</a>"+
+                    "<p>"+price+"</p>"+
+                    "</div>";
+
+            }
+        $('head title').html(ourCategory);
+        $('.breadcrumb .active').html(ourCategory);
+        $('#mainReceiver').append(listOfDevicesDivs);
+        },
+        error: function(){
+            $('h4').append('error');
+        }
+          
+    });
 }
